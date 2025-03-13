ced8d54535aa00352017de629d8b0d5d3f98bd0e
Sulaiman Syed <Sulaiman.Syed@student.kuleuven.be>
2025-03-13 15:29:59 +0100

    ColumnTest

--- src/test/CellTest.java
+++ src/test/CellTest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package test;
 
 import static org.junit.Assert.*;
@@ -9,49 +6,49 @@ import main.Cell;
 import org.junit.Test;
 
 /**
- * 
+ *
  */
 public class CellTest {
 
-	@Test
-	public void cellConstructor_SetsValueCorrectly() {
-		Cell<String> cell = new Cell<>("test");
-		assertEquals("test", cell.getValue());
-	}
+    @Test
+    public void cellConstructor_SetsValueCorrectly() {
+        Cell<String> cell = new Cell<>("test");
+        assertEquals("test", cell.getValue());
+    }
 
-	@Test
-	public void setValue_ChangesCellValueSuccessfully() {
-		Cell<String> cell = new Cell<>("testValue");
-		assertEquals("testValue", cell.getValue());
-		cell.setValue("changedValue");
-		assertEquals("changedValue", cell.getValue());
-	}
+    @Test
+    public void setValue_ChangesCellValueSuccessfully() {
+        Cell<String> cell = new Cell<>("testValue");
+        assertEquals("testValue", cell.getValue());
+        cell.setValue("changedValue");
+        assertEquals("changedValue", cell.getValue());
+    }
 
-	@Test
-	public void cell_HandlesDifferentDataTypes() {
-		// Integer type
-		Cell<Integer> intCell = new Cell<>(42);
-		assertEquals(Integer.valueOf(42), intCell.getValue());
+    @Test
+    public void cell_HandlesDifferentDataTypes() {
+        // Integer type
+        Cell<Integer> intCell = new Cell<>(42);
+        assertEquals(Integer.valueOf(42), intCell.getValue());
 
-		// Double type
-		Cell<Double> doubleCell = new Cell<>(3.14);
-		assertEquals(Double.valueOf(3.14), doubleCell.getValue());
+        // Double type
+        Cell<Double> doubleCell = new Cell<>(3.14);
+        assertEquals(Double.valueOf(3.14), doubleCell.getValue());
 
-		// Boolean type
-		Cell<Boolean> booleanCell = new Cell<>(true);
-		assertEquals(Boolean.TRUE, booleanCell.getValue());
-	}
+        // Boolean type
+        Cell<Boolean> booleanCell = new Cell<>(true);
+        assertEquals(Boolean.TRUE, booleanCell.getValue());
+    }
 
-	@Test
-	public void cell_AllowsNullValues() {
-		Cell<String> cell = new Cell<>(null);
-		assertNull(cell.getValue()); // Check initial null value
+    @Test
+    public void cell_AllowsNullValues() {
+        Cell<String> cell = new Cell<>(null);
+        assertNull(cell.getValue()); // Check initial null value
 
-		cell.setValue("notNull");
-		assertEquals("notNull", cell.getValue()); // Check updated value
+        cell.setValue("notNull");
+        assertEquals("notNull", cell.getValue()); // Check updated value
 
-		cell.setValue(null);
-		assertNull(cell.getValue()); // Check reset to null
-	}
+        cell.setValue(null);
+        assertNull(cell.getValue()); // Check reset to null
+    }
 
 }
--- src/test/ColumnTest.java
+++ src/test/ColumnTest.java
@@ -1,20 +1,99 @@
-/**
- * 
- */
 package test;
 
 import static org.junit.Assert.*;
 
+import main.Cell;
+import main.Column;
 import org.junit.Test;
 
-/**
- * 
- */
+import java.util.List;
+
 public class ColumnTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructorAndGetInfo() {
+		Column<String> column = new Column<>("TestColumn", String.class, true, "Default");
+
+		assertEquals("TestColumn", column.getName());
+		assertEquals(String.class, column.getType());
+		assertTrue(column.getInfo().contains("TestColumn String true Default"));
 	}
 
+	@Test
+	public void testAddCellAndSize() {
+		Column<Integer> column = new Column<>("NumberColumn", Integer.class, false, 42);
+
+		assertEquals(0, column.getSize());
+
+		column.addCell();
+		assertEquals(1, column.getSize());
+
+		column.addCell();
+		assertEquals(2, column.getSize());
+	}
+
+	@Test
+	public void testRemoveCell() {
+		Column<String> column = new Column<>("StringColumn", String.class, true, "Default");
+
+		column.addCell();
+		column.addCell();
+		assertEquals(2, column.getSize());
+
+		column.removeRow(0);
+		assertEquals(1, column.getSize());
+	}
+
+	@Test
+	public void testGetAndSetCell() {
+		Column<String> column = new Column<>("StringColumn", String.class, false, "");
+
+		column.addCell();
+		column.updateCell(0, "stringValue");
+		assertEquals("stringValue", column.getCell(0));
+	}
+
+	@Test
+	public void testChangeAllowsBlanks() {
+		Column<String> column = new Column<>("BlankColumn", String.class, false, "Default");
+
+		assertFalse(column.getInfo().contains("true"));
+
+		column.changeAllowBlanks();
+		assertTrue(column.getInfo().contains("true"));
+
+		column.changeAllowBlanks();
+		assertFalse(column.getInfo().contains("true"));
+	}
+
+	@Test
+	public void testSetName() {
+		Column<String> column = new Column<>("OriginalName", String.class, true, "Default");
+
+		column.setName("NewName");
+		assertEquals("NewName", column.getName());
+	}
+
+	@Test
+	public void testGetColumnContent() {
+		Column<Boolean> column = new Column<>("BooleanColumn", Boolean.class, false, false);
+
+		column.addCell();
+		column.addCell();
+		column.updateCell(0, "true");
+		column.updateCell(1, "false");
+
+		List<String> columnValues = column.getColumn();
+		assertEquals(2, columnValues.size());
+		assertEquals("true", columnValues.get(0));
+		assertEquals("false", columnValues.get(1));
+	}
+
+	@Test
+	public void testDefaultCellContent() {
+		Column<String> column = new Column<>("DefaultTest", String.class, true, "DefaultValue");
+
+		column.addCell();
+		assertEquals("DefaultValue", column.getCell(0));
+	}
 }
