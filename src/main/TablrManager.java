6723786cad64b4ce900ca3ee784b070945085883
Sulaiman Syed <Sulaiman.Syed@student.kuleuven.be>
2025-04-02 14:20:19 +0500

    renamed TablrManagerListener to paintlistener

--- src/main/TablrManager.java
+++ src/main/TablrManager.java
@@ -12,18 +12,18 @@ import java.util.stream.Collectors;
  */
 public class TablrManager {
 	private List<Table> tables;
-	private List<TablrManagerListener> listeners;
+	private List<paintListener> listeners;
 
 	/**
 	 * Constructs a new instance of the TablrManager class.
-	 *
+	 * <p>
 	 * This constructor initializes the TablrManager with an empty list of tables
 	 * and listeners. It also notifies registered listeners of a change in the
 	 * state by invoking the `fireContentsChanged` method.
 	 */
 	public TablrManager() {
 		this.tables = new ArrayList<Table>();
-		listeners = new ArrayList<TablrManagerListener>();
+		listeners = new ArrayList<paintListener>();
 		this.fireContentsChanged();
 	}
 
@@ -33,7 +33,7 @@ public class TablrManager {
 	 *
 	 * @param listener The listener to be added. Must implement the TablrManagerListener interface.
 	 */
-	public void addListener(TablrManagerListener listener) {
+	public void addListener(paintListener listener) {
 		listeners.add(listener);
 	}
 
@@ -43,17 +43,17 @@ public class TablrManager {
 	 *
 	 * @param listener The listener to be removed. Must implement the TablrManagerListener interface.
 	 */
-	public void removeListener(TablrManagerListener listener) {
+	public void removeListener(paintListener listener) {
 		listeners.remove(listener);
 	}
 
 	/**
 	 * Notifies all registered listeners that the contents of the TablrManager
 	 * have been modified. This method iterates through the list of TablrManagerListener
-	 * instances and invokes their {@link TablrManagerListener#contentsChanged()} method.
+	 * instances and invokes their {@link paintListener#contentsChanged()} method.
 	 */
 	private void fireContentsChanged() {
-		for (TablrManagerListener listener : listeners) {
+		for (paintListener listener : listeners) {
 			listener.contentsChanged();
 		}
 	}
@@ -103,7 +103,6 @@ public class TablrManager {
 	}
 
 	/**
-	 * 
 	 * @param tableName The name of the table.
 	 * @return A list with the info of the columns.
 	 */
@@ -113,7 +112,6 @@ public class TablrManager {
 	}
 
 	/**
-	 * 
 	 * @param table   The old name of the table.
 	 * @param newName The new name of the table.
 	 */
@@ -124,7 +122,6 @@ public class TablrManager {
 	}
 
 	/**
-	 *
 	 * @param table   The name of the table.
 	 * @param newName The new name of the table.
 	 */
@@ -135,7 +132,6 @@ public class TablrManager {
 	}
 
 	/**
-	 * 
 	 * @param table The name of the table.
 	 */
 	public void addColumn(String table) {
@@ -145,7 +141,6 @@ public class TablrManager {
 	}
 
 	/**
-	 * 
 	 * @param table The name of the table.
 	 * @return A list of all the names of the columns.
 	 */
@@ -203,10 +198,10 @@ public class TablrManager {
 	 * Updates the value of a cell in a specified table and column at the given row index.
 	 * After updating the cell, this method notifies all registered listeners about the change.
 	 *
-	 * @param nameTable The name of the table containing the cell to update.
+	 * @param nameTable  The name of the table containing the cell to update.
 	 * @param nameColumn The name of the column containing the cell to update.
-	 * @param rowIndex The index of the row containing the cell to update.
-	 * @param value The new value to set in the specified cell.
+	 * @param rowIndex   The index of the row containing the cell to update.
+	 * @param value      The new value to set in the specified cell.
 	 */
 	public void updateCell(String nameTable, String nameColumn, Integer rowIndex, String value) {
 		Table t = findTable(nameTable);
@@ -217,9 +212,9 @@ public class TablrManager {
 	/**
 	 * Retrieves the value of a specific cell in a table.
 	 *
-	 * @param nameTable The name of the table containing the desired cell.
+	 * @param nameTable  The name of the table containing the desired cell.
 	 * @param nameColumn The name of the column containing the desired cell.
-	 * @param rowIndex The index of the row containing the desired cell.
+	 * @param rowIndex   The index of the row containing the desired cell.
 	 * @return The value of the cell as a String.
 	 */
 	public String getCell(String nameTable, String nameColumn, Integer rowIndex) {
@@ -233,7 +228,7 @@ public class TablrManager {
 	 * This method updates the blank allowance setting for the given column and notifies
 	 * all registered listeners about the change.
 	 *
-	 * @param tableName The name of the table containing the column to update.
+	 * @param tableName  The name of the table containing the column to update.
 	 * @param columnName The name of the column whose blank allowance setting will be changed.
 	 */
 	public void changeAllowBlanks(String tableName, String columnName) {
@@ -247,7 +242,7 @@ public class TablrManager {
 	 *
 	 * @param tableName The name of the table to search for.
 	 * @return The Table object that matches the specified name,
-	 *         or null if no table with the given name is found.
+	 * or null if no table with the given name is found.
 	 */
 	private Table findTable(String tableName) {
 		for (Table t : tables) {
@@ -263,7 +258,7 @@ public class TablrManager {
 	 * This method locates the table by its name, updates the type of the given column,
 	 * and notifies all registered listeners of the change.
 	 *
-	 * @param table The name of the table containing the column whose type is to be changed.
+	 * @param table  The name of the table containing the column whose type is to be changed.
 	 * @param column The name of the column whose type will be modified.
 	 */
 	public void changeType(String table, String column) {
@@ -275,7 +270,7 @@ public class TablrManager {
 	/**
 	 * Retrieves the default value of a specific column in a specified table.
 	 *
-	 * @param table The name of the table containing the column.
+	 * @param table  The name of the table containing the column.
 	 * @param column The name of the column for which the default value is to be retrieved.
 	 * @return The default value of the specified column in the table, or null if no default value is set.
 	 */
@@ -287,7 +282,7 @@ public class TablrManager {
 	/**
 	 * Retrieves the class type of the specified column in the specified table.
 	 *
-	 * @param table The name of the table containing the column.
+	 * @param table  The name of the table containing the column.
 	 * @param column The name of the column whose class type is to be retrieved.
 	 * @return The Class object representing the type of the specified column in the table.
 	 */
@@ -301,9 +296,9 @@ public class TablrManager {
 	 * This method locates the table by its name, updates the default value
 	 * of the given column, and notifies all registered listeners of the change.
 	 *
-	 * @param table The name of the table containing the column.
+	 * @param table  The name of the table containing the column.
 	 * @param column The name of the column for which the default value is to be set.
-	 * @param value The default value to assign to the specified column.
+	 * @param value  The default value to assign to the specified column.
 	 */
 	public void setDefaultValue(String table, String column, String value) {
 		Table t = findTable(table);
